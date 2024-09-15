d286173e7fa04df994045324e2846f62c417c58c
maxime-baron <maxime.baron2002@mail.com>
2024-09-15 21:00:51 +0200

    :construction: Wip profile page

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,65 +3,60 @@ import { addUserToGroup } from "./add-user-to-group/resource"
 import { postConfirmation } from "../auth/post-confirmation/resource";
 
 const schema = a.schema({
-  UserProfile: a.model({
-    userId: a.string().required(),
-    profileOwner: a.string().required(),
-    pseudo: a.string().required(),
-    profilePicture: a.url(),
-    conversationUsers: a.hasMany("ConversationUser", "userId"),
-    Players: a.hasMany("Player", "profileId"),
-    pricingPlan: a.string().default("Free"),
-    outgoingFriendRequests: a.hasMany("FriendRequest", "requestId"),
-    incomingFriendRequests: a.hasMany("FriendRequest", "requestId")
-  }).identifier(["userId"]).secondaryIndexes((index) => [index("profileOwner")]).authorization((allow) => [allow.ownerDefinedIn("profileOwner")]),
+  // UserProfile: a.model({
+  //   userId: a.string().required(),
+  //   profileOwner: a.string().required(),
+  //   conversationUsers: a.hasMany("ConversationUser", "userId"),
+  //   Players: a.hasMany("Player", "profileId"),
+  //   pricingPlan: a.string().default("Free"),
+  //   outgoingFriendRequests: a.hasMany("FriendRequest", "requestId"),
+  //   incomingFriendRequests: a.hasMany("FriendRequest", "requestId")
+  // }).identifier(["userId"]).secondaryIndexes((index) => [index("profileOwner")]).authorization((allow) => [allow.ownerDefinedIn("profileOwner")]),
 
   FriendRequest: a.model({
-    requestId: a.id().required(),
-    userUsername: a.string().required(),
-    user: a.belongsTo("UserProfile", "requestId"),
-    friendUsername: a.string().required(),
-    friend: a.belongsTo("UserProfile", "requestId"),
+    targetUserId: a.string().required(),
     status: a.enum(["pending", "accepted", "rejected"]),
-  }).identifier(["requestId"]),
+  }).authorization(allow => [allow.owner().to(['delete', 'create'])]),
 
   Party: a.model({
-    players: a.hasMany("Player", "partyId"),
-    conversation: a.hasOne("Conversation", "partyId"),
-    scores: a.json(),
+    // players: a.hasMany("Player", "partyId"),
+    // conversation: a.hasOne("Conversation", "partyId"),
+    song: a.string().required(),
+    scores: a.float().required(),
     status: a.enum(['Waiting',"Done","InProgress"])
   }),
 
-  Player: a.model({
-    profileId: a.id(),
-    partyId: a.id(),
-    user: a.belongsTo("UserProfile", "profileId"),
-    party: a.belongsTo("Party", "partyId"),
-    charPos: a.integer(),
-    score: a.integer().required().default(0),
-  }),
+  // Player: a.model({
+  //   profileId: a.id(),
+  //   partyId: a.id(),
+  //   user: a.belongsTo("UserProfile", "profileId"),
+  //   party: a.belongsTo("Party", "partyId"),
+  //   charPos: a.integer(),
+  //   score: a.integer().required().default(0),
+  // }),
 
-  Conversation: a.model({
-    partyId: a.id(),
-    party: a.belongsTo("Party", "partyId"),
-    users: a.hasMany("ConversationUser", "conversationId"),
-    messages: a.hasMany("Message", "conversationId"),
-  }),
+  // Conversation: a.model({
+  //   partyId: a.id(),
+  //   party: a.belongsTo("Party", "partyId"),
+  //   users: a.hasMany("ConversationUser", "conversationId"),
+  //   messages: a.hasMany("Message", "conversationId"),
+  // }),
 
-  ConversationUser: a.model({
-    userId: a.string(),
-    conversationId: a.id(),
-    user: a.belongsTo("UserProfile", "userId"),
-    conversation: a.belongsTo("Conversation", "conversationId"),
-    messages: a.hasMany("Message", "userId")
-  }),
+  // ConversationUser: a.model({
+  //   userId: a.string(),
+  //   conversationId: a.id(),
+  //   user: a.belongsTo("UserProfile", "userId"),
+  //   conversation: a.belongsTo("Conversation", "conversationId"),
+  //   messages: a.hasMany("Message", "userId")
+  // }),
 
-  Message: a.model({
-    conversationId: a.id().required(),
-    userId: a.string().required(),
-    content: a.string().required(),
-    sender: a.belongsTo("ConversationUser", "userId"),
-    conversation: a.belongsTo("Conversation", "conversationId")
-  }),
+  // Message: a.model({
+  //   conversationId: a.id().required(),
+  //   userId: a.string().required(),
+  //   content: a.string().required(),
+  //   sender: a.belongsTo("ConversationUser", "userId"),
+  //   conversation: a.belongsTo("Conversation", "conversationId")
+  // }),
 
   //lambda
   addUserToGroup: a
@@ -74,7 +69,7 @@ const schema = a.schema({
     .handler(a.handler.function(addUserToGroup))
     .returns(a.json())
 
-}).authorization((allow) => [allow.authenticated().to(["read"]),allow.owner().to(["create"]),allow.resource(postConfirmation)])
+}).authorization(allow => [allow.resource(postConfirmation)])
 
 export type Schema = ClientSchema<typeof schema>
 
